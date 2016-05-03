7416942fd1cdfa67865eb430374af59ff80a87b4
ja-marin <josef1807@hotmail.com>
2016-05-03 23:02:16 +0200

    Despliegue DB en Heroku

--- models/index.js
+++ models/index.js
@@ -2,11 +2,25 @@ var path =require('path');
 
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
+//Usar BBDD SQlite
+// DATABASE_URL =sqlite:///
+// DATABASE_STORAGE =quiz.squlite
+//Usar BDD Postgres:
+//	DATABASE_URL0 postgres://user:passwd@host:port/database
 
-//Usar BBDD SQLite:
-var sequelize= new Sequelize(null,null,null,
-	{dialect: "sqlite",storage: "public/quiz.sqlite"});
+var url,storage;
+if(!process.env.DATABASE_URL){
+	url = "sqlite:///";
+	storage = "quiz.sqlite";
+}else{
+	url = process.env.DATABASE_URL;
+	storage = process.env.DATABASE_STORAGE || "";
+}
+var sequelize = new Sequelize(url,
+	{storage: storage,
+	omitNull: true	
 
+});
 //Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
@@ -22,7 +36,7 @@ sequelize
 						console.log('Base de datos inicializada con datos');
 					});
 			}
-			else{console.log('No esta vacia');}
+			else{console.log('La base de datos no está vacía');}
 		});
 }).catch(function(error){
 	console.log("Error Sincronizando las tablas de la BBDD:",error);
