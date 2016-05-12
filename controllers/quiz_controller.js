4b555fddcaa86e43a3003cf1db80e87be7c0ce6f
ja-marin <josef1807@hotmail.com>
2016-05-12 14:38:02 +0200

    Tema 12: Crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -64,4 +64,19 @@ exports.check=function(req,res,next){
 	}).catch(function(error){next(error);});
 	
 };
+exports.new=function(req,res,next){
+	var quiz = models.Quiz.build({question: "", answer: ""});
+	res.render('quizzes/new', {quiz: quiz});
+};
 
+exports.create=function(req,res,next){
+	var quiz= models.Quiz.build({question: req.body.quiz.question,
+								answer: req.body.quiz.answer	});
+
+	quiz.save({fields: ["question", "answer"]})
+	.then(function(quiz){
+		res.redirect('/quizzes');
+	}).catch(function(error){
+		next(error);
+	});
+};
